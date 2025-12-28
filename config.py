9c71e91b76d3497155f6b6de988e590eb632234c
Apnimaa <patelkinjal1212@gmail.com>
2025-12-28 16:21:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 ADMINS = [int(id) for id in os.environ.get("ADMINS", "").split(",")]
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://niravpatel180503_db_user:vjWNaWhRk0gMSNyQ@cluster0.26bfgmf.mongodb.net/?appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "REXSaveRestricted")
 LOG_CHANNEL=os.environ.get("LOG_CHANNEL","-1003182485007")
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
