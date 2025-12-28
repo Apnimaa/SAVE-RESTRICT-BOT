8c799d4e6a98aad57e1abeeae7553ec22c8206ca
Apnimaa <patelkinjal1212@gmail.com>
2025-12-28 16:22:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,9 @@
 # Telegram Channel @RexBots_Official
 import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-ADMINS = [int(id) for id in os.environ.get("ADMINS", "").split(",")]
+API_ID = int(os.environ.get("API_ID", "27433400"))
+API_HASH = os.environ.get("API_HASH", "1a286620de5ffe0a7d9b57e604293555")
+ADMINS = [int(id) for id in os.environ.get("ADMINS", "6201066540").split(",")]
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://niravpatel180503_db_user:vjWNaWhRk0gMSNyQ@cluster0.26bfgmf.mongodb.net/?appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "REXSaveRestricted")
 LOG_CHANNEL=os.environ.get("LOG_CHANNEL","-1003182485007")
