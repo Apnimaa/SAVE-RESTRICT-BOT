eb3b29850ba3f4059d36a92727c10c2d68c54c2e
Apnimaa <patelkinjal1212@gmail.com>
2026-01-09 10:24:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_ID = int(os.environ.get("API_ID", "27433400"))
 API_HASH = os.environ.get("API_HASH", "1a286620de5ffe0a7d9b57e604293555")
 ADMINS = [int(id) for id in os.environ.get("ADMINS", "6201066540").split(",")]
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://niravpatel180503_db_user:vjWNaWhRk0gMSNyQ@cluster0.26bfgmf.mongodb.net/?appName=Cluster0")
-DB_NAME = os.environ.get("DB_NAME", "REXSaveRestricted")
+DB_NAME = os.environ.get("DB_NAME", "niravpatel180503_db_user")
 LOG_CHANNEL=os.environ.get("LOG_CHANNEL","-1003182485007")
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
 KEEP_ALIVE_URL = os.environ.get("KEEP_ALIVE_URL", "")
